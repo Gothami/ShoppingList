edd311d9a8fd9992bfd1eb0461f6c289a0d2a356
https://github.com/Gothami <gothamisa92@gmail.com>
2020-02-28 12:08:52 +1030

    Add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { listviewComponent } from './listview/app.listview';
+import { headerComponent } from './header/header.component';
+import { navComponent } from './nav/nav.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +9,10 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navComponent,
+    headerComponent,
+    listviewComponent
   ],
   imports: [
     BrowserModule,
